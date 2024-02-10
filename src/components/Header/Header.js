f7a69357f71775a66fc278c8471196f92f3cff3c
Claren <claren.so@hotmail.com>
2024-02-10 15:21:11 +1100

    Add desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -28,8 +28,25 @@ const Header = () => {
           </ActionGroup>
         </Row>
       </SuperHeader>
+
       <MainHeader>
+        <LaptopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </LaptopActionGroup>
+
         <Logo />
+
+        <SubscribeWrapper>
+          <Button>subscribe</Button>
+          <SubscriberLink href="/subscribe">
+            Already a subscriber?
+          </SubscriberLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +56,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +80,57 @@ const ActionGroup = styled.div`
   }
 `;
 
+const LaptopActionGroup = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    gap: 24px;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    position: relative;
+    display: revert;
+    justify-self: end;
+  }
+`;
+
+const SubscriberLink = styled.a`
+  text-decoration: underline;
+  font-style: italic;
+  color: ${COLORS.gray[900]};
+  font-size: calc((14 / 16) * 1rem);
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  margin-top: 8px;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: revert;
+    justify-items: start;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
 
 export default Header;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 import format from 'date-fns/format';
+import { QUERIES } from '../../constants';
 
 const Logo = (props) => {
   return (
@@ -8,20 +9,27 @@ const Logo = (props) => {
       <Link href="/" {...props}>
         New Grid Times
       </Link>
-      <TodaysDate>
-        {format(new Date(), 'EEEE, MMMM do, yyyy')}
-      </TodaysDate>
+      <TodaysDate>{format(new Date(), 'EEEE, MMMM do, yyyy')}</TodaysDate>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   text-align: center;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 const Link = styled.a`
   font-family: var(--font-family-logo);
   font-size: 3rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: 4rem;
+  }
 `;
 
 const TodaysDate = styled.p`
