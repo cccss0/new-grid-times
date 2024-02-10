26a8b984e6e80cba8b3642a1eac3b34a55d62aa2
Claren <claren.so@hotmail.com>
2024-02-11 00:04:13 +1100

    Add grid layouts for tablet and laptop

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -8,6 +8,7 @@ import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
+import { COLORS, QUERIES } from '../../constants';
 
 const MainStoryGrid = () => {
   return (
@@ -19,18 +20,22 @@ const MainStoryGrid = () => {
       <SecondaryStorySection>
         <StoryList>
           {SECONDARY_STORIES.map((story, index) => (
-            <SecondaryStory key={story.id} {...story} />
+            <VerticalStoryWrapper key={story.id}>
+              <SecondaryStory {...story} />
+            </VerticalStoryWrapper>
           ))}
         </StoryList>
       </SecondaryStorySection>
 
       <OpinionSection>
         <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
+        <OpinionStoryList>
           {OPINION_STORIES.map((story, index) => (
-            <OpinionStory key={story.id} {...story} />
+            <VerticalStoryWrapper key={story.id}>
+              <OpinionStory {...story} />
+            </VerticalStoryWrapper>
           ))}
-        </StoryList>
+        </OpinionStoryList>
       </OpinionSection>
 
       <AdvertisementSection>
@@ -49,14 +54,49 @@ const Wrapper = styled.div`
     'advertisement';
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    padding: 20px;
+    gap: 48px 0px;
+  }
+
+  @media ${QUERIES.tabletOnly} {
+    grid-template-areas:
+      'main-story secondary-stories'
+      'advertisement advertisement'
+      'opinion-stories opinion-stories';
+    grid-template-columns: 2fr 1fr;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas:
+      'main-story secondary-stories opinion-stories'
+      'main-story advertisement advertisement';
+
+    grid-template-columns: 5fr 4fr 3fr;
+    gap: 0px;
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+
+  @media ${QUERIES.tabletAndUp} {
+    padding-right: 20px;
+    margin-right: 20px;
+    border-right: 1px solid var(--color-gray-300);
+  }
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
+  background-color: var(--color-gray-100);
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 20px;
+    margin-right: 20px;
+    border-right: 1px solid var(--color-gray-300);
+  }
 `;
 
 const StoryList = styled.div`
@@ -64,12 +104,47 @@ const StoryList = styled.div`
   flex-direction: column;
 `;
 
+const OpinionStoryList = styled(StoryList)`
+  background-color: var(--color-gray-100);
+  @media ${QUERIES.tabletOnly} {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-column-gap: 30px;
+  }
+`;
+
 const OpinionSection = styled.section`
   grid-area: opinion-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: -8px;
+  }
 `;
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-top: 20px;
+    margin-top: 20px;
+    border-top: 1px solid var(--color-gray-300);
+  }
+`;
+
+const VerticalStoryWrapper = styled.div`
+  &:not(:last-of-type) {
+    padding-bottom: 16px;
+    border-bottom: 1px solid var(--color-gray-300);
+    margin-bottom: 16px;
+  }
+
+  @media ${QUERIES.tabletOnly} {
+    &:not(:last-of-type) {
+      padding-bottom: revert;
+      border-bottom: revert;
+      margin-bottom: revert;
+    }
+  }
 `;
 
 export default MainStoryGrid;
--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 const OpinionStory = ({ id, title, author, avatar }) => {
   return (
     <a href={`/story/${id}`}>
       <Wrapper>
         <Avatar alt="" src={avatar} />
-        <div>
+        <OpinionCaption>
           <AuthorName>{author}</AuthorName>
           <ArticleTitle>{title}</ArticleTitle>
-        </div>
+        </OpinionCaption>
       </Wrapper>
     </a>
   );
@@ -17,6 +18,20 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+
+  @media ${QUERIES.tabletOnly} {
+    flex-direction: column;
+  }
+`;
+
+const OpinionCaption = styled.div`
+  margin-right: auto;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-right: revert;
+  }
 `;
 
 const Avatar = styled.img`
@@ -25,6 +40,11 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  margin-left: 16px;
+
+  @media ${QUERIES.tabletOnly} {
+    margin-left: revert;
+  }
 `;
 
 const AuthorName = styled.p`
